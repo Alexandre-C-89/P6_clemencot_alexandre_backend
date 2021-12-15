f526a74eabae4740b0e3ea2e5cb40cae1a8fdd8a
Alexandre <clemencot.alexandre@gmail.com>
2021-12-15 18:18:47 +0100

    changement limite de connexion

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const app = express();
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5 // limit each IP to 50 requests per windowMs
+  max: 10 // limit each IP to 5 requests per windowMs
 });
 
 
